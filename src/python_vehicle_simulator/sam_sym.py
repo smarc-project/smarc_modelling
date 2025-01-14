c50bd71f085bd5688f5b41365631a0380d1c671f
David <ddorner@kth.se>
2025-01-14 17:43:01 +0100

    Latest test version

--- src/python_vehicle_simulator/sam_sym.py
+++ src/python_vehicle_simulator/sam_sym.py
@@ -12,8 +12,12 @@ eta0 = np.zeros(7)
 eta0[6] = 1.0  # Initial quaternion (no rotation) 
 nu0 = np.zeros(6)  # Zero initial velocities
 ksi0 = np.zeros(6)  # All systems start at zero position
-ksi0[0] = 0.088/2
+#ksi0[0] = 0.088/2
 #ksi0[1] = 0.057/2
+#ksi0[2] = np.deg2rad(5)
+#ksi0[3] = np.deg2rad(5)
+#ksi0[4] = 1525
+#ksi0[5] = 1525
 ksi_dot0 = np.zeros(6)  # All rates start at zero
 x0 = np.concatenate([eta0, nu0, ksi0, ksi_dot0])
 
@@ -25,37 +29,37 @@ t_eval = np.linspace(t_span[0], t_span[1], 1000)
 vbs_pieces = [{
     "interval": (0, t_span[1]),
     "name": "constant",
-    "params": {"val": 0.0},  # No VBS acceleration
+    "params": {"val": 100.0},  # No VBS acceleration
 }]
 
 lcg_pieces = [{
     "interval": (0, t_span[1]),
     "name": "constant",
-    "params": {"val": 0.0},  # No LCG acceleration
+    "params": {"val": 100.0},  # No LCG acceleration
 }]
 
 stern_pieces = [{
     "interval": (0, t_span[1]),
     "name": "constant",
-    "params": {"val": 0.0},  # No stern acceleration
+    "params": {"val": 110.0},  # No stern acceleration
 }]
 
 rudder_pieces = [{
-    "interval": (0, t_span[1]),
+    "interval": (3, 10),
     "name": "constant",
-    "params": {"val": 0.0},  # No rudder acceleration
+    "params": {"val": 100.0},  # No rudder acceleration
 }]
 
 prop1_pieces = [
     {
         "interval": (0, 5),
         "name": "constant",
-        "params": {"val": 0.0},  # Constant acceleration phase
+        "params": {"val": 100.0},  # Constant acceleration phase
     },
     {
         "interval": (5, t_span[1]),
         "name": "constant",
-        "params": {"val": 0.0},  # Zero acceleration phase
+        "params": {"val": 100.0},  # Zero acceleration phase
     }
 ]
 
@@ -63,12 +67,12 @@ prop2_pieces = [
     {
         "interval": (0, 5),
         "name": "constant",
-        "params": {"val": 0.0},  # Opposite acceleration
+        "params": {"val": 100.0},  # Opposite acceleration
     },
     {
         "interval": (5, t_span[1]),
         "name": "constant",
-        "params": {"val": 0.0},  # Zero acceleration phase
+        "params": {"val": 100.0},  # Zero acceleration phase
     }
 ]
 
