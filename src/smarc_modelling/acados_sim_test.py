b5bec63b01072d42a1fd71e812921dc76c9c51e2
CSimonSp <sspang00@gmail.com>
2025-03-10 12:02:20 +0100

    first it. reference and error plots

--- src/smarc_modelling/acados_sim_test.py
+++ src/smarc_modelling/acados_sim_test.py
@@ -17,13 +17,21 @@ from smarc_modelling.lib import *
 from smarc_modelling.vehicles.SAM_casadi import SAM_casadi
 from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver
 
-def plot(x_axis, simX, simU):
-    plt.figure()
-    plt.subplot(4,2,1)
-    plt.plot(x_axis, simX[:,:3])
-    plt.legend(["X", "Y", "Z"])
-    plt.ylabel("Position [m]")
-    plt.grid()
+def plot(x_axis, ref, simX, simU):
+    ref_err = ref
+    ref = ref[:13]  
+
+    q = [ref[3], ref[4], ref[5], ref[6]]
+    psi, theta, phi = gnc.quaternion_to_angles(q)
+
+    reference = np.zeros(12)
+    reference[ :3] = ref[ :3]
+    reference[3:6] = np.array([phi, theta, psi])
+    reference[6:]  = ref[7:]
+    
+    ref = reference
+    for i in range(np.size(simX,0)-1):
+        ref = np.vstack((ref, reference))
 
     n = len(simX)
     psi = np.zeros(n)
@@ -33,52 +41,201 @@ def plot(x_axis, simX, simU):
     for i in range(n):
         q = [simX[i, 3], simX[i, 4], simX[i, 5], simX[i, 6]]
         psi[i], theta[i], phi[i] = gnc.quaternion_to_angles(q)
+    
+    print(np.shape(simX))
+    y_axis = simX[:, 0]
+    for i in range(np.size(simX, 1)):
+        if i in [3, 4, 5, 6]:
+            if i == 3:
+                y_axis = np.column_stack((y_axis, phi))
+            elif i == 4:
+                y_axis = np.column_stack((y_axis, theta))
+            elif i == 5:
+                y_axis = np.column_stack((y_axis, psi))
+        else: 
+            y_axis = np.column_stack((y_axis, simX[:,i]))
+            
 
-
-    plt.subplot(4,2,2)
-    plt.plot(x_axis, np.rad2deg(psi), x_axis, np.rad2deg(theta), x_axis, np.rad2deg(phi))
-    plt.legend(["roll", "pitch", "yaw"])
-    plt.ylabel("Angle [deg]")
+    plt.figure()
+    plt.subplot(4,3,1)
+    plt.plot(x_axis, simX[:, 0] )
+    plt.plot(x_axis,  ref[:, 0], linestyle='--', color='r')
+    plt.legend(["X", "X_ref"])
+    plt.ylabel("Position [m]")
     plt.grid()
 
-    plt.subplot(4,2,3)
-    plt.plot(x_axis, simX[:,7:10])
-    plt.legend(["u", "v", "w"])
-    plt.ylabel("Velocity [m/s]")
+    plt.subplot(4,3,2)
+    plt.plot(x_axis, simX[:, 1] )
+    plt.plot(x_axis,  ref[:, 1], linestyle='--', color='r')
+    plt.legend(["Y", "Y_ref"])
+    plt.ylabel("Position [m]")
     plt.grid()
 
-    plt.subplot(4,2,4)
-    plt.plot(x_axis, simX[:,10:13])
-    plt.legend(["Roll", "Pitch", "Yaw"])
-    plt.ylabel("Angular velocity")
+    plt.subplot(4,3,3)
+    plt.plot(x_axis, simX[:, 2])
+    plt.plot(x_axis,  ref[:, 2], linestyle='--', color='r')
+    plt.legend(["Z", "Z_ref"])
+    plt.ylabel("Position [m]")
     plt.grid()
 
-    plt.subplot(4,2,5)
-    plt.step(x_axis, simX[:,13:17])
-    plt.legend(["VBS", "LCG", "d_s", "d_r"])
-    plt.ylabel("Control 1")
+    plt.subplot(4,3,4)
+    plt.plot(x_axis, simX[:, 7])
+    plt.plot(x_axis,  ref[:, 6], linestyle='--', color='r')
+    plt.legend([r"$\dotX$", r"$\dotX_{ref}$"])
+    plt.ylabel("X Velocity [m/s]")
     plt.grid()
 
-    plt.subplot(4,2,6)
-    plt.step(x_axis, simX[:,17:19])
-    plt.legend(["RPM1", "RPM2"])
-    plt.ylabel("Control 2")
+    plt.subplot(4,3,5)
+    plt.plot(x_axis, simX[:, 8])
+    plt.plot(x_axis,  ref[:, 7], linestyle='--', color='r')
+    plt.legend([r"$\dotY$", r"$\dotY_{ref}$"])
+    plt.ylabel("Y Velocity [m/s]")
     plt.grid()
 
-    plt.subplot(4,2,7)
-    plt.step(x_axis[:-1], simU[:,:4])
-    plt.legend(["VBS", "LCG", "d_s", "d_r"])
-    plt.ylabel("Control derivative")
-    plt.xlabel("Time [s]")
+    plt.subplot(4,3,6)
+    plt.plot(x_axis, simX[:, 9])
+    plt.plot(x_axis,  ref[:, 8], linestyle='--', color='r')
+    plt.legend([r"$\dotZ$", r"$\dotZ_{ref}$"])
+    plt.ylabel("Z Velocity [m/s]")
     plt.grid()
 
-    plt.subplot(4,2,8)
-    plt.step(x_axis[:-1], simU[:,4:])
-    plt.legend(["RPM1", "RPM2"])
-    plt.ylabel("Control derivative")
-    plt.xlabel("Time [s]")
+    plt.subplot(4,3,7)
+    plt.plot(x_axis, np.rad2deg(phi))
+    plt.plot(x_axis, ref[:, 3], linestyle='--', color='r')
+    plt.legend([r"$\phi$", r"$\phi_{ref}$"])
+    plt.ylabel("Roll [deg]")    
+    plt.grid()
+
+    plt.subplot(4,3,8)
+    plt.plot(x_axis, np.rad2deg(theta))
+    plt.plot(x_axis, ref[:, 4], linestyle='--', color='r')
+    plt.legend([r"$\theta$", r"$\theta_{ref}$"])
+    plt.ylabel("Pitch [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,9)
+    plt.plot(x_axis, np.rad2deg(psi))
+    plt.plot(x_axis, ref[:, 5], linestyle='--', color='r')
+    plt.legend([r"$\psi$", r"$\psi_{ref}$"])
+    plt.ylabel("Yaw [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,10)
+    plt.plot(x_axis, simX[:, 10])
+    plt.plot(x_axis,  ref[:, 9], linestyle='--', color='r')
+    plt.legend([r"$\dot\phi$", r"$\dot\phi_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
+    plt.grid()
+
+    plt.subplot(4,3,11)
+    plt.plot(x_axis, simX[:, 11])
+    plt.plot(x_axis,  ref[:, 10], linestyle='--', color='r')
+    plt.legend([r"$\dot\theta$", r"$\dot\theta_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
+    plt.grid()
+
+    plt.subplot(4,3,12)
+    plt.plot(x_axis, simX[:, 12])
+    plt.plot(x_axis,  ref[:, 11], linestyle='--', color='r')
+    plt.legend([r"$\dot\psi$", r"$\dot\psi_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
     plt.grid()
     plt.show()
+
+    print(np.shape(y_axis))
+    x_error = RMSE_calculation(y_axis, ref)
+
+    # Error plots
+    plt.figure()
+    plt.subplot(4,3,1)
+    plt.plot(x_axis, x_error[:, 0] )
+    plt.legend(["X"])
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(4,3,2)
+    plt.plot(x_axis, x_error[:, 1] )
+    plt.legend(["Y"])
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(4,3,3)
+    plt.plot(x_axis, x_error[:, 2])
+    plt.legend(["Z"])
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(4,3,4)
+    plt.plot(x_axis, x_error[:, 3])
+    plt.legend([r"$\phi error$"])
+    plt.ylabel("Roll [deg]")    
+    plt.grid()
+
+    plt.subplot(4,3,5)
+    plt.plot(x_axis, x_error[:, 4])
+    plt.legend([r"$\theta error$"])
+    plt.ylabel("Pitch [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,6)
+    plt.plot(x_axis, x_error[:, 5])
+    plt.legend([r"$\psi error$"])
+    plt.ylabel("Yaw [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,7)
+    plt.plot(x_axis[:-1], simU[:, 0])
+    plt.legend([r"$VBS$"])
+    plt.title("Control inputs")
+    plt.ylabel("VBS input") 
+    plt.grid()
+
+    plt.subplot(4,3,8)
+    plt.plot(x_axis[:-1], simU[:, 2])
+    plt.legend([r"$Stern angle$"])
+    plt.ylabel(r"Stern Input [$\degree$]")
+    plt.grid()
+
+    plt.subplot(4,3,9)
+    plt.plot(x_axis[:-1], simU[:, 4])
+    plt.legend([r"RPM1$"])
+    plt.ylabel("Motor RPM")
+    plt.grid()
+
+    plt.subplot(4,3,10)
+    plt.plot(x_axis[:-1], simU[:, 1])
+    plt.legend([r"$LCG$"])
+    plt.ylabel("LCG input")
+    plt.grid()
+
+    plt.subplot(4,3,11)
+    plt.plot(x_axis[:-1], simU[:, 3])
+    plt.legend([r"$Rudder angle$"])
+    plt.ylabel(r"Rudder Input [$\degree$]")
+    plt.grid()
+
+    plt.subplot(4,3,12)
+    plt.plot(x_axis[:-1], simU[:, 5])
+    plt.legend([r"RPM2$"])
+    plt.ylabel("Motor RPM")
+    plt.grid()
+
+    plt.show()
+
+def RMSE_calculation(var, ref):
+    cumulative_rmse = np.empty(np.shape(var))
+    for i in range(np.size(ref,1)-1):      # Loop over all references
+        sum_of_squared_errors = 0
+        rmse_k = np.empty([])
+        for k in range(np.size(var, 0)-1):  # Loop over all time steps
+            # Calculate squared error for the current time step
+            squared_error = (var[k, i] - ref[k, i])**2
+            sum_of_squared_errors += squared_error
+            
+            # Calculate cumulative RMSE
+            rmse_k = np.append(rmse_k, np.sqrt(sum_of_squared_errors / (k+1)))
+        cumulative_rmse[:,i] = rmse_k
+    return cumulative_rmse
 
 
 def setup(x0, N_horizon, Tf, model, ocp):
@@ -213,9 +370,9 @@ def main():
 
     # Horizon parameters 
     Ts = 0.1            # Sampling Time
-    N_horizon = 12       # Prediction Horizon
+    N_horizon = 10       # Prediction Horizon
     Tf = Ts*N_horizon
-    Nsim = 400          # Simulation duration (no. of iterations) - sim. length is Ts*Nsim
+    Nsim = 1000          # Simulation duration (no. of iterations) - sim. length is Ts*Nsim
 
 
     # Setup of the solver and integrator
@@ -266,7 +423,7 @@ def main():
 
     # plot results
     x_axis = np.linspace(0, (Tf/N_horizon)*Nsim, Nsim+1)
-    plot(x_axis, simX, simU)
+    plot(x_axis, ref, simX, simU)
 
     ocp_solver = None
 
