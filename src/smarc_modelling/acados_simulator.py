45f8ff01ad81c915af194f7b151cceb491c34e6b
CSimonSp <sspang00@gmail.com>
2025-03-11 11:57:54 +0100

    Added NMPC to control script, and cleaned up in acados simulator

---
+++ src/smarc_modelling/acados_simulator.py
@@ -0,0 +1,359 @@
+#---------------------------------------------------------------------------------
+# INFO:
+# Script to test the acados framework before putting it into the other scripts.
+# It is based on the acados example minimal_example_closed_loop.py in getting started
+# The NMPC base will exist in this script
+#---------------------------------------------------------------------------------
+import sys
+import os
+# Add the src directory to the system path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
+import numpy as np
+import casadi as ca
+import matplotlib.pyplot as plt
+from control import *
+
+from smarc_modelling.vehicles import *
+from smarc_modelling.lib import *
+from smarc_modelling.vehicles.SAM_casadi import SAM_casadi
+
+def plot(x_axis, ref, simX, simU):
+    ref = ref[:13]  
+
+    q = [ref[3], ref[4], ref[5], ref[6]]
+    psi, theta, phi = gnc.quaternion_to_angles(q)
+
+    reference = np.zeros(12)
+    reference[ :3] = ref[ :3]
+    reference[3:6] = np.array([phi, theta, psi])
+    reference[6:]  = ref[7:]
+    
+    ref = reference
+    for i in range(np.size(simX,0)-1):
+        ref = np.vstack((ref, reference))
+
+    n = len(simX)
+    psi = np.zeros(n)
+    theta = np.zeros(n)
+    phi = np.zeros(n)
+
+    for i in range(n):
+        q = [simX[i, 3], simX[i, 4], simX[i, 5], simX[i, 6]]
+        psi[i], theta[i], phi[i] = gnc.quaternion_to_angles(q)
+    
+    y_axis = np.zeros(np.shape(simX))
+    for i in range(np.size(simX, 1)):
+        if i in [3, 4, 5, 6]:
+            if i == 3:
+                y_axis[:,i] = phi
+            elif i == 4:
+                y_axis[:,i] = theta
+            elif i == 5:
+                y_axis[:,i] = psi
+        else: 
+            y_axis[:,i] = simX[:,i]
+            
+
+    plt.figure()
+    plt.subplot(4,3,1)
+    plt.plot(x_axis, simX[:, 0] )
+    plt.plot(x_axis,  ref[:, 0], linestyle='--', color='r')
+    plt.legend(["X", "X_ref"])
+    plt.ylabel("Position [m]")
+    plt.grid()
+
+    plt.subplot(4,3,2)
+    plt.plot(x_axis, simX[:, 1] )
+    plt.plot(x_axis,  ref[:, 1], linestyle='--', color='r')
+    plt.legend(["Y", "Y_ref"])
+    plt.ylabel("Position [m]")
+    plt.grid()
+
+    plt.subplot(4,3,3)
+    plt.plot(x_axis, simX[:, 2])
+    plt.plot(x_axis,  ref[:, 2], linestyle='--', color='r')
+    plt.legend(["Z", "Z_ref"])
+    plt.ylabel("Position [m]")
+    plt.grid()
+
+    plt.subplot(4,3,4)
+    plt.plot(x_axis, simX[:, 7])
+    plt.plot(x_axis,  ref[:, 6], linestyle='--', color='r')
+    plt.legend([r"$\dotX$", r"$\dotX_{ref}$"])
+    plt.ylabel("X Velocity [m/s]")
+    plt.grid()
+
+    plt.subplot(4,3,5)
+    plt.plot(x_axis, simX[:, 8])
+    plt.plot(x_axis,  ref[:, 7], linestyle='--', color='r')
+    plt.legend([r"$\dotY$", r"$\dotY_{ref}$"])
+    plt.ylabel("Y Velocity [m/s]")
+    plt.grid()
+
+    plt.subplot(4,3,6)
+    plt.plot(x_axis, simX[:, 9])
+    plt.plot(x_axis,  ref[:, 8], linestyle='--', color='r')
+    plt.legend([r"$\dotZ$", r"$\dotZ_{ref}$"])
+    plt.ylabel("Z Velocity [m/s]")
+    plt.grid()
+
+    plt.subplot(4,3,7)
+    plt.plot(x_axis, np.rad2deg(phi))
+    plt.plot(x_axis, ref[:, 3], linestyle='--', color='r')
+    plt.legend([r"$\phi$", r"$\phi_{ref}$"])
+    plt.ylabel("Roll [deg]")    
+    plt.grid()
+
+    plt.subplot(4,3,8)
+    plt.plot(x_axis, np.rad2deg(theta))
+    plt.plot(x_axis, ref[:, 4], linestyle='--', color='r')
+    plt.legend([r"$\theta$", r"$\theta_{ref}$"])
+    plt.ylabel("Pitch [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,9)
+    plt.plot(x_axis, np.rad2deg(psi))
+    plt.plot(x_axis, ref[:, 5], linestyle='--', color='r')
+    plt.legend([r"$\psi$", r"$\psi_{ref}$"])
+    plt.ylabel("Yaw [deg]")
+    plt.grid()
+
+    plt.subplot(4,3,10)
+    plt.plot(x_axis, simX[:, 10])
+    plt.plot(x_axis,  ref[:, 9], linestyle='--', color='r')
+    plt.legend([r"$\dot\phi$", r"$\dot\phi_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
+    plt.grid()
+
+    plt.subplot(4,3,11)
+    plt.plot(x_axis, simX[:, 11])
+    plt.plot(x_axis,  ref[:, 10], linestyle='--', color='r')
+    plt.legend([r"$\dot\theta$", r"$\dot\theta_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
+    plt.grid()
+
+    plt.subplot(4,3,12)
+    plt.plot(x_axis, simX[:, 12])
+    plt.plot(x_axis,  ref[:, 11], linestyle='--', color='r')
+    plt.legend([r"$\dot\psi$", r"$\dot\psi_{ref}$"])
+    plt.ylabel("Angular Velocity [rad/s]")
+    plt.grid()
+
+    x_error = RMSE_calculation(y_axis, ref)
+
+    # Error plots
+    plt.figure()
+    plt.subplot(2,3,1)
+    plt.plot(x_axis, x_error[:, 0] )
+    plt.legend(["X"])
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(2,3,2)
+    plt.plot(x_axis, x_error[:, 1] )
+    plt.legend(["Y"])
+    plt.title("Errors")
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(2,3,3)
+    plt.plot(x_axis, x_error[:, 2])
+    plt.legend(["Z"])
+    plt.ylabel("Position RMSE [m]")
+    plt.grid()
+
+    plt.subplot(2,3,4)
+    plt.plot(x_axis, x_error[:, 3])
+    plt.legend([r"$\phi$ error"])
+    plt.ylabel("Roll [deg]")    
+    plt.grid()
+
+    plt.subplot(2,3,5)
+    plt.plot(x_axis, x_error[:, 4])
+    plt.legend([r"$\theta$ error"])
+    plt.ylabel("Pitch [deg]")
+    plt.grid()
+
+    plt.subplot(2,3,6)
+    plt.plot(x_axis, x_error[:, 5])
+    plt.legend([r"$\psi$ error"])
+    plt.ylabel("Yaw [deg]")
+    plt.grid()
+
+    # Control Inputs
+    plt.figure()
+    plt.subplot(4,3,1)
+    plt.plot(x_axis, simX[:, 13])
+    plt.legend([r"VBS"])
+    plt.ylabel("VBS input") 
+    plt.grid()
+
+    plt.subplot(4,3,2)
+    plt.plot(x_axis, simX[:, 15])
+    plt.title("Control inputs")
+    plt.legend([r"Stern angle"])
+    plt.ylabel(r" Degrees [$\degree$]")
+    plt.grid()
+
+    plt.subplot(4,3,3)
+    plt.plot(x_axis, simX[:, 17])
+    plt.legend([r"RPM1"])
+    plt.ylabel("Motor RPM")
+    plt.grid()
+
+    plt.subplot(4,3,4)
+    plt.plot(x_axis[:-1], simU[:, 0])
+    plt.legend([r"VBS"])
+    plt.ylabel("VBS derivative") 
+    plt.grid()
+
+    plt.subplot(4,3,5)
+    plt.plot(x_axis[:-1], simU[:, 2])
+    plt.legend([r"Stern angle"])
+    plt.ylabel(r" Degree derivative [$\degree/s$]")
+    plt.grid()
+
+    plt.subplot(4,3,6)
+    plt.plot(x_axis[:-1], simU[:, 4])
+    plt.legend([r"RPM1"])
+    plt.ylabel("RPM1 derivative")
+    plt.grid()
+
+    plt.subplot(4,3,7)
+    plt.plot(x_axis, simX[:, 14])
+    plt.legend([r"LCG"])
+    plt.ylabel("LCG input")
+    plt.grid()
+
+    plt.subplot(4,3,8)
+    plt.plot(x_axis, simX[:, 16])
+    plt.legend([r"Rudder angle"])
+    plt.ylabel(r" degrees [$\degree$]")
+    plt.grid()
+
+    plt.subplot(4,3,9)
+    plt.plot(x_axis, simX[:, 18])
+    plt.legend([r"RPM2"])
+    plt.ylabel("Motor RPM")
+    plt.grid()
+
+    plt.subplot(4,3,10)
+    plt.plot(x_axis[:-1], simU[:, 1])
+    plt.legend([r"LCG"])
+    plt.ylabel("LCG derivative") 
+    plt.grid()
+
+    plt.subplot(4,3,11)
+    plt.plot(x_axis[:-1], simU[:, 3])
+    plt.legend([r"Rudder angle"])
+    plt.ylabel(r" Degree derivative [$\degree/s$]")
+    plt.grid()
+
+    plt.subplot(4,3,12)
+    plt.plot(x_axis[:-1], simU[:, 5])
+    plt.legend([r"RPM2"])
+    plt.ylabel("RPM2 derivative")
+    plt.grid()
+
+    plt.show()
+
+    print(f"RMSE for each variable: {x_error[-1, :6]}")
+
+def RMSE_calculation(var, ref):
+    cumulative_rmse = np.empty(np.shape(var))
+    for i in range(np.size(ref,1)-1):      # Loop over all references
+        sum_of_squared_errors = 0
+        rmse_k = np.empty([])
+        for k in range(np.size(var, 0)-1):  # Loop over all time steps
+            # Calculate squared error for the current time step
+            squared_error = (var[k, i] - ref[k, i])**2
+            sum_of_squared_errors += squared_error
+            
+            # Calculate cumulative RMSE
+            rmse_k = np.append(rmse_k, np.sqrt(sum_of_squared_errors / (k+1)))
+        cumulative_rmse[:,i] = rmse_k
+    return cumulative_rmse
+
+def main():
+    # Extract the CasADi model
+    sam = SAM_casadi()
+    model = sam.export_dynamics_model()
+    nx = model.x.rows()
+    nu = model.u.rows()
+    Nsim = 1000          # Simulation duration (no. of iterations) - sim. length is Ts*Nsim
+    simU = np.zeros((Nsim, nu))     # Matrix to store the optimal control sequence
+    simX = np.zeros((Nsim+1, nx))   # Matrix to store the simulated state
+
+    # create ocp object to formulate the OCP
+    Ts = 0.1
+    N_horizon = 10
+    nmpc = NMPC(model, Ts, N_horizon)
+
+    # Declare the initial state
+    x0 = np.zeros(nx)
+    x0[0] = 0 
+    x0[3] = 1       # Must be 1 (quaternions)
+    x0[17:] = 1e-6
+    x0[7]   = 1e-6
+    # x0[13] = 50
+    # x0[14] = 50
+    simX[0,:] = x0
+
+    # Declare the reference state - Static point in this tests
+    ref = np.zeros((nx + nu,))
+    ref[0] = 0.2
+    ref[1] = 0.0
+    ref[2] = 0.0
+    ref[3] = 1
+
+    # Setup of the solver and integrator
+    ocp_solver, integrator = nmpc.setup(x0)
+
+    # Array to store the time values
+    t = np.zeros((Nsim))
+
+    # Initialize the state and control vector as David does
+    for stage in range(N_horizon + 1):
+        ocp_solver.set(stage, "x", x0)
+    for stage in range(N_horizon):
+        ocp_solver.set(stage, "u", np.zeros(nu,))
+
+
+    # closed loop - simulation
+    for i in range(Nsim):
+        # Update reference vector
+        for stage in range(N_horizon):
+            ocp_solver.set(stage, "yref", ref)
+        ocp_solver.set(N_horizon, "yref", ref[:nx])
+
+        # Set current state
+        ocp_solver.set(0, "lbx", simX[i, :])
+        ocp_solver.set(0, "ubx", simX[i, :])
+
+        # solve ocp and get next control input
+        status = ocp_solver.solve()
+        #ocp_solver.print_statistics()
+        if status != 0:
+            print(f" Note: acados_ocp_solver returned status: {status}")
+
+        # simulate system
+        t[i] = ocp_solver.get_stats('time_tot')
+        simU[i, :] = ocp_solver.get(0, "u")
+        X_eval = ocp_solver.get(0, "x")
+        print(f"Nsim: {i}\nX_opt {X_eval[13:]}")
+        simX[i+1, :] = integrator.simulate(x=simX[i, :], u=simU[i, :])
+
+    # evaluate timings
+    t *= 1000  # scale to milliseconds
+    print(f'Computation time in ms:\n min: {np.min(t):.3f}\nmax: {np.max(t):.3f}\navg: {np.average(t):.3f}\nstdev: {np.std(t)}\nmedian: {np.median(t):.3f}')
+
+
+    # plot results
+    x_axis = np.linspace(0, (Ts)*Nsim, Nsim+1)
+    plot(x_axis, ref, simX, simU)
+
+    ocp_solver = None
+
+
+if __name__ == '__main__':
+    main()
--- src/smarc_modelling/control.py
+++ src/smarc_modelling/control.py
@@ -1,64 +1,116 @@
 # Script for the acados NMPC model
-from acados_template import AcadosOcp, AcadosOcpSolver
+from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver
 import numpy as np
 import casadi as ca
 
 class NMPC:
-    def solve(self, model):
-        self.ocp = AcadosOcp()
-        self.model = model  # Must be an acados ocp-model
+    def __init__(self, model, Ts, N_horizon):
+        '''
+        Input:
+        model == Casadi model
+        Ts == Sampling Time
+        N_horizon == control horizon
+        '''
+        self.ocp   = AcadosOcp()
+        self.model = model
+        self.ocp.model = self.model
+        self.Ts    = Ts
+        self.Tf    = Ts*N_horizon
+        self.N_horizon = N_horizon
         
-        # Horizon parameters
-        N = 20
-        Tf = 1.0
-        self.ocp.solver_options.N_horizon = N
-        self.ocp.solver_options.tf = Tf
-
-        # Declaration of cost matrices
+    def setup(self, x0):
         nx = self.model.x.rows()
         nu = self.model.u.rows()
-        print(nx, nu)
-        Q = np.eye(nx)
-        R = np.eye(nu)
-        
-        # path cost
+
+        # --------------------------- Cost setup ---------------------------------
+        # State weight matrix
+        Q_diag = np.ones(nx)
+        Q_diag[ 0:3 ] = 4e3
+        Q_diag[ 3:7 ] = 4e3
+        Q_diag[ 7:10] = 500
+        Q_diag[10:13] = 500
+
+        # Control weight matrix - Costs set according to Bryson's rule (MPC course)
+        Q_diag[13:15] = 1e-6
+        Q_diag[15:17] = 1/50
+        Q_diag[17:  ] = 1e-6
+        Q = np.diag(Q_diag)
+
+        # Control rate of change weight matrix - control inputs as [x_vbs, x_lcg, delta_s, delta_r, rpm1, rpm2]
+        R_diag = np.ones(nu)
+        R_diag[ :2] = 4e-2
+        R_diag[2:4] = 1
+        R_diag[4: ] = 1e-5
+        R = np.diag(R_diag)
+
+        # Stage costs
+        ref = np.zeros((nx + nu,))
+        self.ocp.cost.yref  = ref        # Init ref point. The true references are declared in the sim. for-loop
         self.ocp.cost.cost_type = 'NONLINEAR_LS'
-        self.ocp.model.cost_y_expr = ca.vertcat(self.model.x, self.model.u)
-        self.ocp.cost.yref = np.zeros((nx+nu))
         self.ocp.cost.W = ca.diagcat(Q, R).full()
-
-        # terminal cost
+        self.ocp.model.cost_y_expr = ca.vertcat(self.model.x, self.model.u)
+        
+        # Terminal cost
         self.ocp.cost.cost_type_e = 'NONLINEAR_LS'
-        self.ocp.cost.yref_e = np.zeros((nx))
+        self.ocp.cost.W_e = Q#np.zeros(np.shape(Q))
         self.ocp.model.cost_y_expr_e = self.model.x
-        self.ocp.cost.W_e = Q
+        self.ocp.cost.yref_e = ref[:nx]
 
-        # set constraints
-        self.ocp.constraints.x0 = np.array([0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        # --------------------- Constraint Setup --------------------------
+        vbs_dot = 10    # Maximum rate of change for the VBS
+        lcg_dot = 15    # Maximum rate of change for the LCG
+        ds_dot  = 7     # Maximum rate of change for stern angle
+        dr_dot  = 7     # Maximum rate of change for rudder angle
+        rpm_dot = 1000  # Maximum rate of change for rpm
 
-        # set options
-        self.ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
-        # PARTIAL_CONDENSING_HPIPM, FULL_CONDENSING_QPOASES, FULL_CONDENSING_HPIPM,
-        # PARTIAL_CONDENSING_QPDUNES, PARTIAL_CONDENSING_OSQP, FULL_CONDENSING_DAQP
-        self.ocp.solver_options.hessian_approx = 'GAUSS_NEWTON' # 'GAUSS_NEWTON', 'EXACT'
+        # Declare initial state
+        self.ocp.constraints.x0 = x0
+
+        # Set constraints on the control rate of change
+        self.ocp.constraints.lbu = np.array([-vbs_dot,-lcg_dot, -ds_dot, -dr_dot, -rpm_dot, -rpm_dot])
+        self.ocp.constraints.ubu = np.array([ vbs_dot, lcg_dot,  ds_dot,  dr_dot,  rpm_dot,  rpm_dot])
+        self.ocp.constraints.idxbu = np.arange(nu)
+
+        # Set constraints on the states
+        x_ubx = np.ones(nx)
+        x_ubx[  :13] = 1000
+
+        # Set constraints on the control
+        x_ubx[13:15] = 100 
+        x_ubx[15:17] = 7
+        x_ubx[17:  ] = 1000
+
+        x_lbx = -x_ubx
+        x_lbx[13:15] = 0
+
+        self.ocp.constraints.lbx = x_lbx
+        self.ocp.constraints.ubx = x_ubx
+        self.ocp.constraints.idxbx = np.arange(nx)
+
+        # ----------------------- Solver Setup --------------------------
+        # set prediction horizon
+        self.ocp.solver_options.N_horizon = self.N_horizon
+        self.ocp.solver_options.tf = self.Tf
+
+        self.ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
+        self.ocp.solver_options.hpipm_mode = 'ROBUST'
+        self.ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
         self.ocp.solver_options.integrator_type = 'IRK'
-        # ocp.solver_options.print_level = 1
-        self.ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
-        self.ocp.solver_options.globalization = 'MERIT_BACKTRACKING' # turns on globalization
+        self.ocp.solver_options.sim_method_newton_iter = 3 #3 default
 
-        ocp_solver = AcadosOcpSolver(self.ocp)
+        self.ocp.solver_options.nlp_solver_type = 'SQP_RTI'
+        self.ocp.solver_options.nlp_solver_max_iter = 80
+        self.ocp.solver_options.tol    = 1e-6       # NLP tolerance. 1e-6 is default for tolerances
+        self.ocp.solver_options.qp_tol = 1e-6       # QP tolerance
 
-        status = ocp_solver.solve()
-        ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
+        self.ocp.solver_options.globalization = 'MERIT_BACKTRACKING'
+        self.ocp.solver_options.regularize_method = 'NO_REGULARIZE'
 
-        if status != 0:
-            raise Exception(f'acados returned status {status}.')
 
-        # Create two empty arrays to store the solution in
-        x_opt_values = np.zeros((N+1, nx))
-        u_opt_values = np.zeros((N, nu))
-        # Extract the values from the solution
-        for i in range(N):
-            x_opt_values[i,:] = ocp_solver.get(i, "x")
-            u_opt_values[i,:] = ocp_solver.get(i, "u")
-        x_opt_values[N,:] = ocp_solver.get(N, "x")
+        solver_json = 'acados_ocp_' + self.model.name + '.json'
+        acados_ocp_solver = AcadosOcpSolver(self.ocp, json_file = solver_json)
+
+        # create an integrator with the same settings as used in the OCP solver.
+        acados_integrator = AcadosSimSolver(self.ocp, json_file = solver_json)
+
+        return acados_ocp_solver, acados_integrator
