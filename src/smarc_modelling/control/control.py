Fixing numerical instabilities for the MPC
Still not quite there yet, but it's better and it runs. Changed the RPM gain because suddenly the propellers go much gentler, probably because I changed the nozzle and the propellers don't scratch it anymore.

src/smarc_modelling/control/control.py
@@ -68,7 +68,7 @@ class NMPC:
         Q_diag[14] = 1e-5            # LCG:      Standard: 1e-4
         Q_diag[ 15  ] = 5e2             # stern_angle:   Standard: 100
         Q_diag[ 16  ] = 1e3             # rudder_angle:  Standard: 100
-        Q_diag[17:  ] = 1e-5            # RPM1 And RPM2: Standard: 1e-6
+        Q_diag[17:  ] = 1e-6            # RPM1 And RPM2: Standard: 1e-6
         Q_diag[13:  ] = Q_diag[13:  ]   # Adjustment to all control weights
         Q = np.diag(Q_diag)
 